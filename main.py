efb034824fa1d3f9daf2d4a3ea3e7eea8a4e06e5
Lange99 <c.langellotti@studenti.unibs.it>
2022-06-30 14:36:48 +0200

    get weather from lan and lon

--- main.py
+++ main.py
@@ -5,14 +5,27 @@ import os
 import weatherRequest
 
 # simple reply bot
-
+users = {}
 
 def handle(msg):
+    global users
     chat_id = msg['chat']['id']
-    command = msg['text']
-    weatherRequest.weather_request(msg['text'])
-    print(command)
-    bot.sendMessage(chat_id, command)
+
+    if msg.get('location'):
+        users[chat_id] = msg['location']
+        lat = msg['location']['latitude']
+        lon = msg['location']['longitude']
+        weather = weatherRequest.weather_request(lat, lon)
+        print(weather)
+        bot.sendMessage(chat_id, "poition saved")
+    else:
+        pass
+        #print hello world and user guide
+        #print(command)
+        #city = command.split(' ')[1]
+        #response = weatherRequest.weather_request(city)
+        #print(response)
+        bot.sendMessage(chat_id, "hello world and user guide")
 
 
 telegram_token = os.environ["TELEGRAM_TOKEN"]
--- weatherRequest.py
+++ weatherRequest.py
@@ -1,11 +1,19 @@
+from datetime import datetime
 import requests
 import json
 import os
 
-def weather_request(city):
+def weather_request(lat, lon):
     appid = os.environ["OPENWEATHER_APPID"]
-    url = "http://api.openweathermap.org/data/2.5/forecast?q=" + city +"&appid=" + appid
+    url = "http://api.openweathermap.org/data/2.5/forecast?lat=" + str(lat) + "&lon=" + str(lon) + "&appid=" + appid
     response = requests.get(url)
     data = response.json()
-    print(data)
     return data
+
+def parse_weather(data):
+    weathers = data["list"]
+    today = datetime.today().date()
+    for weather in weathers:
+        weather_date = datetime.fromtimestamp(weather["dt"]).date()
+        if weather_date == today:
+            #
